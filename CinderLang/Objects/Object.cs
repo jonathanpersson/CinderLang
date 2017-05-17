856ba9f2fb2effdfc6f66dd374392a91f2dec142
Jonathan Persson <Jonathan Persson>
2017-05-17 18:57:25 +0200

    Add project files.

---
+++ CinderLang/Objects/Object.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinderLang.Objects
+{
+    class Object
+    {
+        private string _identifier = "";
+        public string Identifier { get { return _identifier; } set { _identifier = value; } }
+        public Dictionary<string, dynamic> Subs = new Dictionary<string, dynamic>();
+        public Dictionary<int, List<string>> Args = new Dictionary<int, List<string>>();
+    }
+}
