bdddfe7642ed611ac0e390c5ef6d45d560f6c7c3
Jonathan Persson <jonathan_persson@outlook.com>
2017-06-04 17:48:39 +0200

    Started working on objects.

--- CinderLang/Objects/Object.cs
+++ CinderLang/Objects/Object.cs
@@ -8,9 +8,33 @@ namespace CinderLang.Objects
 {
     class Object
     {
-        private string _identifier = "";
-        public string Identifier { get { return _identifier; } set { _identifier = value; } }
-        public Dictionary<string, dynamic> Subs = new Dictionary<string, dynamic>();
-        public Dictionary<int, List<string>> Args = new Dictionary<int, List<string>>();
+        // Fields.
+        private string _identifier = ""; // Identifier field.
+        private Dictionary<string, dynamic> _subs = new Dictionary<string, dynamic>(); // Subs field. Used to store subruitines in object.
+        private Dictionary<int, List<string>> _args = new Dictionary<int, List<string>>(); // Args field.
+        private Dictionary<int, List<string>> _lines = new Dictionary<int, List<string>>(); // Lines field.
+
+        // Properties.
+        public string Identifier { get { return _identifier; } set { _identifier = value; } } // Identifier property.
+
+        // Add subs to _subs.
+        public void add_subs (Dictionary<string, dynamic> subs_to_add, bool debug_out = false)
+        {
+            foreach (string sub_id in subs_to_add.Keys)
+            {
+                _subs.Add(sub_id, subs_to_add[sub_id]);
+                if (debug_out == true) { Console.WriteLine($"Sub \"{sub_id}\" added to \"{_identifier}\"."); }
+            }
+        }
+
+        // Remove subs from _subs.
+        public void remove_subs (Dictionary<string, dynamic> subs_to_remove, bool debug_out = false)
+        {
+            foreach (string sub_id in subs_to_remove.Keys)
+            {
+                _subs.Remove(sub_id);
+                if (debug_out == true) { Console.WriteLine($"Sub \"{sub_id}\" removed from \"{_identifier}\"."); }
+            }
+        }
     }
 }
